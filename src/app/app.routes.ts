690d30c27c2c8a2ca099daaa849b0c33a92b0aa1
Ali najafian <alinajafian580@gmail.com>
2024-06-14 15:34:00 +0330

    Added basic routing (improvements later)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: LandingPageComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: '**', redirectTo: 'home', pathMatch: 'full'}
+];
