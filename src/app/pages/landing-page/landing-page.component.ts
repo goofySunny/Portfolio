7574c0bdaf2fc578921cf846af229af06249d2e1
Ali najafian <alinajafian580@gmail.com>
2024-06-18 17:57:59 +0330

    Imported CommonModule and NgFor directive

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgFor],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
